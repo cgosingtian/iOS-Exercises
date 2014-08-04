aa0d5d0cc84f37dc2ac7318af0b202cf8b4310d6
Chase Gosingtian <c.gosingtian@klab.com>
2014-08-04 17:41:08 +0800

    Initial commit for chapter 8 exercise (starting Homepwner app)

---
+++ Chapter 8/Homepwner/Homepwner/KLBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KLBAppDelegate.h
+//  Homepwner
+//
+//  Created by Chase Gosingtian on 8/4/14.
+//  Copyright (c) 2014 KLab Cyscorpions, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KLBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
