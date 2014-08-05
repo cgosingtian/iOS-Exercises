a4ad8bcfbc22ff0c98c8ce0298dd440a3f533424
Chase Gosingtian <c.gosingtian@klab.com>
2014-08-05 14:36:21 +0800

    Initial commit for chapter 10. Starting work on a UINavigationController.

---
+++ Chapter 10/Homepwner/Homepwner/KLBItemsViewController.h
@@ -0,0 +1,18 @@
+//
+//  KLBItemsViewController.h
+//  Homepwner
+//
+//  Created by Chase Gosingtian on 8/4/14.
+//  Copyright (c) 2014 KLab Cyscorpions, Inc. All rights reserved.
+//
+
+//#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface KLBItemsViewController : UITableViewController <UITableViewDataSource>
+
+-(void)addRandomItem;
+
+- (IBAction)addNewItem:(id)sender;
+- (IBAction)toggleEditingMode:(id)sender;
+@end
---
+++ Chapter 10/Homepwner/Homepwner/KLBItemsViewController.m
@@ -0,0 +1,192 @@
+//
+//  KLBItemsViewController.m
+//  Homepwner
+//
+//  Created by Chase Gosingtian on 8/4/14.
+//  Copyright (c) 2014 KLab Cyscorpions, Inc. All rights reserved.
+//
+
+#import "KLBItemsViewController.h"
+#import "KLBItem.h"
+#import "KLBItemStore.h"
+
+@interface KLBItemsViewController ()
+@property (nonatomic, strong) IBOutlet UIView *headerView; //strong because top-level view; weak otherwise
+@end
+
+@implementation KLBItemsViewController
+
+-(instancetype)init
+{
+    self = [super initWithStyle:UITableViewStylePlain];
+    if (self) {
+//        for (int i = 0; i < 5; i++) {
+//            [[KLBItemStore sharedStore] createItem];
+//        }
+        
+    }
+    return self;
+}
+
+-(instancetype)initWithStyle:(UITableViewStyle)style
+{
+    return [self init];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [[[KLBItemStore sharedStore] allItems] count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView
+         cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Create an instance of UITableViewCell, with default appearance
+//    UITableViewCell *cell =
+//    [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault
+//                           reuseIdentifier:@"UITableViewCell"];
+    
+    // Get a new or recycled cell
+    UITableViewCell *cell =
+    [tableView dequeueReusableCellWithIdentifier:@"UITableViewCell"
+                                    forIndexPath:indexPath];
+    
+    // Set the text on the cell with the description of the item
+    // that is at the nth index of items, where n = row this cell
+    // will appear in on the tableview
+    NSArray *items = [[KLBItemStore sharedStore] allItems];
+    KLBItem *item = items[indexPath.row];
+    cell.textLabel.text = [item description];
+    return cell;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    [self.tableView registerClass:[UITableViewCell class]
+           forCellReuseIdentifier:@"UITableViewCell"];
+    UIImageView *bg = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"logo.png"]];
+    
+    UIView *header = self.headerView;
+    [self.tableView setTableHeaderView:header];
+    
+    [self.tableView setBackgroundView:bg];
+}
+
+- (void)addRandomItem
+{
+    KLBItem *newItem = [[KLBItemStore sharedStore] createItem];
+    
+    //We could just tell the table to refresh itself...
+    //[(UITableView *)self.view reloadData];
+    
+    ///...or we could have the table animate the insertion of the new item:
+    
+    // Figure out where that item is in the array
+    NSInteger lastRow = [[[KLBItemStore sharedStore] allItems] indexOfObject:newItem];
+    NSIndexPath *indexPath = [NSIndexPath indexPathForRow:lastRow inSection:0];
+    // Insert this new row into the table
+    [self.tableView insertRowsAtIndexPaths:@[indexPath]
+                          withRowAnimation:UITableViewRowAnimationTop];
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if ([indexPath row] == [[[KLBItemStore sharedStore] allItems] count] - 1)
+    {
+        return 44.0;
+    }
+    else return 60.0;
+}
+
+- (void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (indexPath.row != [[[KLBItemStore sharedStore] allItems] count] - 1)
+    {
+        [[cell textLabel] setFont:[UIFont fontWithName:nil size:20]];
+    }
+}
+
+- (UIView *)headerView
+{
+    // If you have not loaded the headerView yet...
+    if (!_headerView) {
+        // Load HeaderView.xib
+        [[NSBundle mainBundle] loadNibNamed:@"HeaderView"
+                                      owner:self
+                                    options:nil];
+    }
+    
+    if (!_headerView) NSLog(@"nullll");
+    return _headerView;
+}
+
+- (IBAction)addNewItem:(id)sender
+{
+    [self addRandomItem];
+}
+
+- (IBAction)toggleEditingMode:(id)sender
+{
+    // If you are currently in editing mode...
+    if (self.isEditing) {
+        // Change text of button to inform user of state
+        [sender setTitle:@"Edit" forState:UIControlStateNormal];
+        // Turn off editing mode
+        [self setEditing:NO animated:YES];
+    } else {
+        // Change text of button to inform user of state
+        [sender setTitle:@"Done" forState:UIControlStateNormal];
+        // Enter editing mode
+        [self setEditing:YES animated:YES];
+    }
+}
+
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // If the table view is asking to commit a delete command...
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        NSArray *items = [[KLBItemStore sharedStore] allItems];
+        KLBItem *item = items[indexPath.row];
+        [[KLBItemStore sharedStore] removeItem:item];
+        // Also remove that row from the table view with an animation
+        [tableView deleteRowsAtIndexPaths:@[indexPath]
+                         withRowAnimation:UITableViewRowAnimationFade];
+    }
+}
+
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (indexPath.row == [[[KLBItemStore sharedStore] allItems] count]-1)
+        return false;
+    else return true;
+}
+
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)sourceIndexPath toIndexPath:(NSIndexPath *)destinationIndexPath
+{
+    if (destinationIndexPath.row != [[[KLBItemStore sharedStore] allItems] count]-1)
+    {
+        [[KLBItemStore sharedStore] moveItemAtIndex:sourceIndexPath.row
+                                            toIndex:destinationIndexPath.row];
+    }
+}
+
+- (NSIndexPath *)tableView:(UITableView *)tableView targetIndexPathForMoveFromRowAtIndexPath:(NSIndexPath *)sourceIndexPath toProposedIndexPath:(NSIndexPath *)proposedDestinationIndexPath
+{
+    //prevent user from moving an item to the last row that has "No more items!" message
+    if(proposedDestinationIndexPath.row == [[[KLBItemStore sharedStore] allItems] count]-1)
+    {
+        return sourceIndexPath;
+    }
+    else
+    {
+        return proposedDestinationIndexPath;
+    }
+}
+
+//bronze challenge
+-(NSString *)tableView:(UITableView *)tableView titleForDeleteConfirmationButtonForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return @"Remove";
+}
+
+@end
