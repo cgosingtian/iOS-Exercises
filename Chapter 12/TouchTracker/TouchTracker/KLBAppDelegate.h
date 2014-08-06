8a21c26c3c45dd7a027c5d7a20d28b655b66f770
Chase Gosingtian <c.gosingtian@klab.com>
2014-08-06 11:02:37 +0800

    Initial commit for chapter 12 exercise: TouchTracker

---
+++ Chapter 12/TouchTracker/TouchTracker/KLBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KLBAppDelegate.h
+//  TouchTracker
+//
+//  Created by Chase Gosingtian on 8/6/14.
+//  Copyright (c) 2014 KLab Cyscorpions, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KLBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
