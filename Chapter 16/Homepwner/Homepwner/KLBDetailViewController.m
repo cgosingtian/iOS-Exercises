ecc304c654325fe9902b97ca9ee4c06b4a6ebbe1
Chase Gosingtian <c.gosingtian@klab.com>
2014-08-07 11:00:56 +0800

    Chapter 16 initial commit.

---
+++ Chapter 16/Homepwner/Homepwner/KLBDetailViewController.m
@@ -0,0 +1,149 @@
+//
+//  KLBDetailViewController.m
+//  Homepwner
+//
+//  Created by Chase Gosingtian on 8/5/14.
+//  Copyright (c) 2014 KLab Cyscorpions, Inc. All rights reserved.
+//
+
+#import "KLBDetailViewController.h"
+#import "KLBDateViewController.h"
+#import "KLBItem.h"
+#import "KLBImageStore.h"
+
+@interface KLBDetailViewController () <UINavigationControllerDelegate,UIImagePickerControllerDelegate,UITextFieldDelegate>
+@property (weak, nonatomic) IBOutlet UITextField *nameField;
+@property (weak, nonatomic) IBOutlet UITextField *serialField;
+@property (weak, nonatomic) IBOutlet UITextField *valueField;
+@property (weak, nonatomic) IBOutlet UILabel *dateLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (weak, nonatomic) IBOutlet UIToolbar *toolBar;
+
+@end
+
+@implementation KLBDetailViewController
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    
+    KLBItem *item = self.item;
+    self.nameField.text = item.itemName;
+    self.serialField.text = item.serialNumber;
+    self.valueField.text = [NSString stringWithFormat:@"%d",item.valueInDollars];
+    
+    [self.valueField setKeyboardType:UIKeyboardTypeNumberPad];
+    
+    self.imageView.image = [[KLBImageStore sharedStore] imageForKey:item.itemKey];
+    
+    static NSDateFormatter *dateFormatter;
+    if (!dateFormatter)
+    {
+        dateFormatter = [[NSDateFormatter alloc]init];
+        dateFormatter.dateStyle = NSDateFormatterMediumStyle;
+        dateFormatter.timeStyle = NSDateFormatterNoStyle;
+    }
+    
+    self.dateLabel.text = [dateFormatter stringFromDate:item.dateCreated];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+    // Clear first responder
+    [self.view endEditing:YES];
+    // "Save" changes to item
+    KLBItem *item = self.item;
+    item.itemName = self.nameField.text;
+    item.serialNumber = self.serialField.text;
+    item.valueInDollars = [self.valueField.text intValue];
+}
+
+- (void)setItem:(KLBItem *)item
+{
+    _item = item;
+    self.navigationItem.title = item.itemName;
+}
+
+//- (void) touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+//{
+//    //[self.valueField resignFirstResponder];
+//    [self.view endEditing:YES];
+//}
+- (IBAction)changeDate:(id)sender {
+    KLBDateViewController *dvc = [[KLBDateViewController alloc]init];
+    
+    dvc.item = self.item;
+    
+    [self.navigationController pushViewController:dvc animated:YES];
+}
+
+- (IBAction)takePicture:(id)sender {
+    UIImagePickerController *picControl = [[UIImagePickerController alloc]init];
+    
+    // If the device has a camera, take a picture, otherwise,
+    // just pick from photo library
+    if ([UIImagePickerController
+         isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+        picControl.sourceType = UIImagePickerControllerSourceTypeCamera;
+    } else {
+        picControl.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    }
+    
+    picControl.delegate = self;
+    picControl.allowsEditing = YES;
+    
+    //present image picker to screen
+    [self presentViewController:picControl animated:YES completion:NULL];
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker
+didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+    // Get picked image from info dictionary
+    UIImage *image = info[UIImagePickerControllerEditedImage];
+    // Put that image onto the screen in our image view
+    self.imageView.image = image;
+    
+    [[KLBImageStore sharedStore] setImage:image forKey:self.item.itemKey];
+    
+    // Take image picker off the screen -
+    // you must call this dismiss method
+    [self dismissViewControllerAnimated:YES completion:NULL];
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    [textField resignFirstResponder];
+    return YES;
+}
+- (IBAction)backgroundTapped:(id)sender {
+    [self.view endEditing:YES];
+}
+- (IBAction)clearImage:(id)sender {
+    self.imageView.image = nil;
+    [[KLBImageStore sharedStore] deleteImageForKey:self.item.itemKey];
+}
+
+//- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+//{
+//    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+//    if (self) {
+//        // Custom initialization
+//    }
+//    return self;
+//}
+//
+//- (void)viewDidLoad
+//{
+//    [super viewDidLoad];
+//    // Do any additional setup after loading the view from its nib.
+//}
+//
+//- (void)didReceiveMemoryWarning
+//{
+//    [super didReceiveMemoryWarning];
+//    // Dispose of any resources that can be recreated.
+//}
+
+@end
