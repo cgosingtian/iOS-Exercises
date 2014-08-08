d421f04b5800d10a7d4faa266138c3394803a49d
Chase Gosingtian <c.gosingtian@klab.com>
2014-08-08 08:36:09 +0800

    Chapter 19 initial commit.

---
+++ Chapter 19/Homepwner/Homepwner/KLBItem.h
@@ -0,0 +1,46 @@
+//
+//  BNRItem.h
+//  RandomItems
+//
+//  Created by John Gallagher on 1/12/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KLBItem : NSObject <NSCoding>
+{
+    NSString *_itemName;
+    NSString *_serialNumber;
+    int _valueInDollars;
+    NSDate *_dateCreated;
+}
+
+@property (nonatomic, copy) NSString *itemKey;
+
++ (instancetype)randomItem;
+
+// Designated initializer for BNRItem
+- (instancetype)initWithItemName:(NSString *)name
+                  valueInDollars:(int)value
+                    serialNumber:(NSString *)sNumber;
+
+- (instancetype)initWithItemName:(NSString *)name;
+
+- (void)setItemName:(NSString *)str;
+- (NSString *)itemName;
+
+- (void)setSerialNumber:(NSString *)str;
+- (NSString *)serialNumber;
+
+- (void)setValueInDollars:(int)v;
+- (int)valueInDollars;
+
+- (void)setDateCreated:(NSDate *)date;
+- (NSDate *)dateCreated;
+
+#pragma mark - NSCoding Protocol
+- (void)encodeWithCoder:(NSCoder *)aCoder;
+- (instancetype)initWithCoder:(NSCoder *)aDecoder;
+
+@end
