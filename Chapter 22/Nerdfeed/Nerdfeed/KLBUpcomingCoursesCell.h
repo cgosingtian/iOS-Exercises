5441786d9216a57ef1d5ef3a05e9eac3d80ae318
Chase Gosingtian <c.gosingtian@klab.com>
2014-08-11 16:10:50 +0800

    Chapter 22 initial commit.

---
+++ Chapter 22/Nerdfeed/Nerdfeed/KLBUpcomingCoursesCell.h
@@ -0,0 +1,15 @@
+//
+//  KLBUpcomingCoursesCell.h
+//  Nerdfeed
+//
+//  Created by Chase Gosingtian on 8/11/14.
+//  Copyright (c) 2014 KLab Cyscorpions, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KLBUpcomingCoursesCell : UITableViewCell
+@property (nonatomic,weak) IBOutlet UILabel *courseLabel;
+@property (nonatomic,weak) IBOutlet UILabel *upcomingLabel;
+
+@end
---
+++ Chapter 22/Nerdfeed/Nerdfeed/KLBUpcomingCoursesCell.m
@@ -0,0 +1,31 @@
+//
+//  KLBUpcomingCoursesCell.m
+//  Nerdfeed
+//
+//  Created by Chase Gosingtian on 8/11/14.
+//  Copyright (c) 2014 KLab Cyscorpions, Inc. All rights reserved.
+//
+
+#import "KLBUpcomingCoursesCell.h"
+
+@implementation KLBUpcomingCoursesCell
+
+@synthesize courseLabel,upcomingLabel;
+
+//- (instancetype) initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+//{
+//    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+//    
+//    if (self)
+//    {
+//        NSLog(@"INSTANTIATED");
+//        upcomingLabel = [[UILabel alloc] init];
+//        upcomingLabel.text = @"coming soon";
+//        NSLog(@"%@",upcomingLabel.text);
+//        courseLabel.text = @"course";
+//        NSLog(@"%@",courseLabel.text);
+//    }
+//    return self;
+//}
+
+@end
